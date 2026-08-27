5e7940fa2cf0e5dbf1d40fa874f787487b34fa07
Javad zargar <zargarjavad013@gmail.com>
2026-08-27 11:32:58 +0330

    Upload file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CanvasEditor from "./components/CanvasEditor";
+
+const App = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900">
+      <CanvasEditor />
+    </div>
+  );
+};
+
+export default App;
